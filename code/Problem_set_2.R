a5b1cabf8a869c3998c93f42df5dfed9a92ceb21
juancamilobejarano <jc.bejaranoc1@uniandes.edu.co>
2023-09-27 07:33:35 -0500

    Update Problem_set_2.R

--- code/Problem_set_2.R
+++ code/Problem_set_2.R
@@ -1,36 +1,48 @@
 ## Rafael Sanchez 202123676 Maria Jose Medellin 202122960 Juan Camilo Bejarano 202121927
-## R version 4.31
+## R version 4.3.1 (2023-06-16 ucrt)
 ## Problem Set 2 
-require(pacman) ## se llama a la biblioteca pacman 
-p_load(rio,data.table,tidyverse) ## se usa p load para llamar a las librerias especificadas 
 
-## Importar exportar bases de datos 
-location= import(file="Input/Módulo de sitio o ubicación.dta")
-identification=import(file = "Input/Módulo de identificación.dta")
+
+require(pacman) ## instalar/llamar pacman 
+ 
+p_load(rio 
+       ,data.table
+       ,tidyverse) ## usar p_load para instalar/llamar librerias especificadas 
+
+## IMPORTAR/EXPORTAR BASE DE DATOS 
+
+## Importar
+location= import(file="Input/Módulo de sitio o ubicación.dta") # importa base de datos 'Modulo de sitio' en objeto 'location'
+
+identification=import(file = "Input/Módulo de identificación.dta") # importa base de datos 'Modulo de identificacion' en objeto 'identification'
 
 ## Exportar
-export(x=location,"output/location.rds")
-export(x=identification,"output/identification.rds")
+export(x=location,"output/location.rds") # exporta objeto 'location' como 'location.rds'(formato R)
 
-##Generar variables 
+export(x=identification,"output/identification.rds") # exporta objeto 'identification' como 'identification.rds' (formato R)
+
+
+##GENERAR VARIABLES
+
 identification= mutate(identification, bussiness_type= case_when(GRUPOS4=="01"~"Agriculutra",
                                                                  GRUPOS4=="02"~"Industria manufacturera",
                                                                  GRUPOS4=="03"~"comercio",
                                                                  GRUPOS4=="04" ~"servicios"))
 
 
-location$local = ifelse(test=location$P3053==6|location$P3053==7 , yes=1, no =0) 
+location$local = ifelse(test=location$P3053==6|location$P3053==7 , yes=1, no =0) # del objeto 'location' genera variable 'local' que es igual a '1' si la variable P3053 es 6 o 7. De lo contrario es '0'.
 
 
-##Eliminar filas columnas de un conjunto de datos 
+##ELIMINAR FILAS/COLUMNAS DE UN CONJUNTO DE DATOS 
 
-identification_sub= subset(x=identification, bussiness_type=="Industria manufacturera")
+identification_sub= subset(x=identification, bussiness_type=="Industria manufacturera") 
 
-location_sub= select(.data=location[1:length(location)], "DIRECTORIO","SECUENCIA_P","SECUENCIA_ENCUESTA","P3054","P469", "COD_DEPTO","F_EXP") 
+location_sub= select(.data=location[1:length(location)], "DIRECTORIO","SECUENCIA_P","SECUENCIA_ENCUESTA","P3054","P469", "COD_DEPTO","F_EXP") # del objeto 'location' selecciona variables y las guarda en nuevo objeto 'location_sub'.
 
-vars=c("DIRECTORIO","SECUENCIA_P","SECUENCIA_ENCUESTA","P35","P241","P3032_1","P3032_2","P3032_3", "P3033","P3034")
-identification_sub=select(.data=identification_sub[1:length(identification_sub)],all_of(vars))
+vars=c("DIRECTORIO","SECUENCIA_P","SECUENCIA_ENCUESTA","P35","P241","P3032_1","P3032_2","P3032_3", "P3033","P3034") # vector con las variables de interes
+identification_sub=select(.data=identification_sub[1:length(identification_sub)],all_of(vars)) #
 
-## Juntar base de datos 
 
-datos=bind_rows (identification_sub,location_sub, .DIRECTORIO="a" ) 
+##COMBINAR BASE DE DATOS 
+
+datos=bind_rows (identification_sub,location_sub, .DIRECTORIO="a" ) #
