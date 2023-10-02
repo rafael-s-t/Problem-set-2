2809b392ec18febdcff1e628ec69fe693a3441ff
juancamilobejarano <jc.bejaranoc1@uniandes.edu.co>
2023-10-01 23:50:43 -0500

    Update Problem_set_2.R

--- code/Problem_set_2.R
+++ code/Problem_set_2.R
@@ -27,7 +27,7 @@ export(x=identification,"output/identification.rds") # exporta objeto 'identific
 identification= mutate(identification, bussiness_type= case_when(GRUPOS4=="01"~"Agriculutra",
                                                                  GRUPOS4=="02"~"Industria manufacturera",
                                                                  GRUPOS4=="03"~"comercio",
-                                                                 GRUPOS4=="04" ~"servicios"))
+                                                                 GRUPOS4=="04" ~"servicios")) # sobre el objeto 'identification' se crean las variables 'Agricultura', 'Industria manufacturera', 'comercio' y 'servicios'
 
 
 location$local = ifelse(test=location$P3053==6|location$P3053==7 , yes=1, no =0) # del objeto 'location' genera variable 'local' que es igual a '1' si la variable P3053 es 6 o 7. De lo contrario es '0'.
@@ -35,13 +35,13 @@ location$local = ifelse(test=location$P3053==6|location$P3053==7 , yes=1, no =0)
 
 ##ELIMINAR FILAS/COLUMNAS DE UN CONJUNTO DE DATOS 
 
-identification_sub= subset(x=identification, bussiness_type=="Industria manufacturera") 
+identification_sub= subset(x=identification, bussiness_type=="Industria manufacturera") # crea un subset con el conjunto de observaciones que pertenecen a la industria manufacturera.
 
 location_sub= select(.data=location[1:length(location)], "DIRECTORIO","SECUENCIA_P","SECUENCIA_ENCUESTA","P3054","P469", "COD_DEPTO","F_EXP") # del objeto 'location' selecciona variables y las guarda en nuevo objeto 'location_sub'.
 
 vars=c("DIRECTORIO","SECUENCIA_P","SECUENCIA_ENCUESTA","P35","P241","P3032_1","P3032_2","P3032_3", "P3033","P3034") # vector con las variables de interes
-identification_sub=select(.data=identification_sub[1:length(identification_sub)],all_of(vars)) #
+identification_sub=select(.data=identification_sub[1:length(identification_sub)],all_of(vars)) # sobrescribe 'identification_sub' con las variables de interes
 
 
 ##COMBINAR BASE DE DATOS 
-datos=merge(location_sub,identification_sub, by=c("DIRECTORIO","SECUENCIA_P","SECUENCIA_ENCUESTA")) #
+datos=merge(location_sub,identification_sub, by=c("DIRECTORIO","SECUENCIA_P","SECUENCIA_ENCUESTA")) # une en una unica base de datos para las variables de interes los objetos 'location_sub' y 'identification_sub'
